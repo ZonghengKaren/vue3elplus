bb4b2596003d019e9ed5bac8c17f460e67f29546
ZonghengKaren <31526975+ZonghengKaren@users.noreply.github.com>
2023-03-14 02:56:45 +0800

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{state.data}}
+    <div @click="bindClick">新增</div>
+    <div @click="$router.push('/about')">跳转</div>
+    <el-button type="primary" @click.stop="goAbout">事件跳转</el-button>
+    <div v-for="item in datalist" :key="item.id">
+      <div>{{item.username}}</div>
+      <div>{{item.phone}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref, onMounted} from "vue";
+import service from "@/utils/http";
+import router from '@/router'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },
+  setup() {
+    let state = ref({});
+    let datalist = ref([]);
+    let datalist2 = ref([]);
+
+    onMounted(() => {
+      state.value.data = 1;
+      getdata();
+      getdata2();
+    })
+
+    let getdata = async () => {
+        const res = await service.get('/users');
+        console.log(res);
+        datalist.value = res.data;
+        const arr = datalist.value.map((item) => {return item.username});
+        console.log(arr);
+    }
+
+    let getdata2 = async () => {
+      const res = await service.get('/posts');
+      datalist2.value = res.data;
+      console.log(datalist2);
+    }
+
+    const bindClick = () => {
+      state.value.data += 2
+    }
+
+    /**
+     * 跳转函数 - 跳转到关于我们
+     */
+    const goAbout = () => {
+      router.push('/about')
+    }
+
+    return {
+      state,
+      datalist,
+      bindClick,
+      goAbout
+    }
   }
 }
 </script>
