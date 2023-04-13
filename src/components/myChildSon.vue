7c0c268549b9357bd35bcff6f5aeb9898039f6ba
ZonghengKaren <31526975+ZonghengKaren@users.noreply.github.com>
2023-04-13 11:26:37 +0800

    Update myChildSon.vue

--- src/components/myChildSon.vue
+++ src/components/myChildSon.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="myChild">
-    <div>count: {{state.count}}</div>
-    <div @click="changUpdate">使用update 修改父级数据</div>
+  <div>
+    <div>这是myChildSon</div>
+    <div class="myChild">
+      <div>count: {{state.count}}</div>
+      <div @click="changUpdate">使用update 修改父级数据</div>
+    </div>
+
+    <div>--------------------------------------------------</div>
+    <el-button text @click="state.dialogVisible = true">
+      click to open the Dialog  | {{state.dialogVisible}}
+    </el-button>
+    <el-dialog
+        v-model="state.dialogVisible"
+        title="Tips"
+        width="30%"
+    >
+      <span>This is a message</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="state.dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="state.dialogVisible = false">
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
-
 <script setup>
-
 const state = reactive({
   count:1,
   data: {
     a: '222'
   },
-  arr:[3333]
+  arr:[3333],
+  dialogVisible: false
 })
 
 const emits = defineEmits(['update:changesyncData'])
